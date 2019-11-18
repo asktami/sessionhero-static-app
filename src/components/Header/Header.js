56f0df1cb8fab6e0120f46c730a1fc7ad8053a9f
Tami Williams <tami@asktami.com>
2019-11-18 14:56:09 -0500

    added with Router to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,23 +3,13 @@ import { Link } from 'react-router-dom';
 import TokenService from '../../services/token-service';
 import '../../index.css';
 
-export default class Header extends Component {
-	handleLogoutClick = () => {
-		TokenService.clearAuthToken();
-	};
+// to get props in Header
+import { withRouter } from 'react-router';
 
-	handleScheduleClick = () => {
-		// TBD
-	};
-
-	render() {
-		console.log('props = ', JSON.stringify(this.props));
+class Header extends Component {
+	renderMessage() {
 		return (
-			<header className="hero">
-				<h1>
-					<Link to={`/`}>SessionHero</Link>
-				</h1>
-
+			<>
 				<p>
 					Search for conference sessions, add conference sessions to your
 					schedule, comment on sessions, and see comments made by others.
@@ -33,11 +23,25 @@ export default class Header extends Component {
 				<p>
 					Demo Username: demoUser
 					<br />
-					Demo Password: demoUser!23
+					Demo Password: demoUser123*
 				</p>
+			</>
+		);
+	}
+	render() {
+		console.log('HEADER props = ', this.props);
 
+		return (
+			<header className="hero">
+				<h1>
+					<Link to={`/`}>SessionHero</Link>
+				</h1>
+
+				{this.renderMessage()}
 				<p>&nbsp;</p>
 			</header>
 		);
 	}
 }
+
+export default withRouter(Header);
