91def1ba20dc7c7e1ec0ec8e62fbd273e68f75af
Tami Williams <tami@asktami.com>
2019-11-21 14:53:05 -0500

    added development notes

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -38,88 +38,99 @@ class SearchBar extends Component {
 			expandAll
 		} = this.context;
 		return (
-			<form className="search-bar">
-				<div>
-					{this.props.location.pathname === '/schedule' ? (
-						<Link to="/schedule" id="btn-show-all" onClick={clearFilters}>
-							Show All
+			<>
+				<form className="search-bar">
+					<div>
+						{this.props.location.pathname === '/schedule' ? (
+							<Link to="/schedule" id="btn-show-all" onClick={clearFilters}>
+								Show All
+							</Link>
+						) : (
+							<Link to="/" id="btn-show-all" onClick={clearFilters}>
+								Show All
+							</Link>
+						)}
+					</div>
+					<div>
+						<select
+							id="filter-day"
+							name="filter-day"
+							aria-label="Select by Day"
+							value={filterDay}
+							onChange={e => setFilterDay(e.target.value)}
+						>
+							<option value="">Select by Day</option>
+							<option value="mon" data-filter="mon">
+								Monday
+							</option>
+							<option value="tue" data-filter="tue">
+								Tuesday
+							</option>
+							<option value="wed" data-filter="wed">
+								Wednesday
+							</option>
+							<option value="thu" data-filter="thu">
+								Thursday
+							</option>
+						</select>
+					</div>
+					<div>
+						<select
+							id="filter-track"
+							name="filter-track"
+							aria-label="Select by Track"
+							value={filterTrack}
+							onChange={e => setFilterTrack(e.target.value)}
+						>
+							<option value="">Select by Track</option>
+							<option value="training" data-filter="training">
+								Training Day
+							</option>
+							<option value="general" data-filter="general">
+								General
+							</option>
+							<option value="create" data-filter="create">
+								Create
+							</option>
+							<option value="share" data-filter="share">
+								Share
+							</option>
+							<option value="integrate" data-filter="integrate">
+								Integrate
+							</option>
+							<option value="business" data-filter="business">
+								Business
+							</option>
+							<option value="sponsor" data-filter="sponsor">
+								Sponsor
+							</option>
+							<option value="fba" data-filter="fba">
+								FBA
+							</option>
+						</select>
+					</div>
+					<div>
+						<Link
+							to={
+								this.props.location.pathname === '/schedule' ? '/schedule' : '/'
+							}
+							id="btn-expand-all"
+							onClick={e => toggleExpandAll(e.target.value)}
+						>
+							{expandAll ? 'Collapse All' : 'Expand All'}
 						</Link>
-					) : (
-						<Link to="/" id="btn-show-all" onClick={clearFilters}>
-							Show All
-						</Link>
-					)}
+					</div>
+				</form>
+				<br />
+				<div className="developer-note">
+					Note: during this test - clicking on the "Sessions" link loads default
+					data - overriding any records added/removed from schedule by clicking
+					on the stars
+					<br />
+					Reloading the browser window also resets everything to the default
+					data.
 				</div>
-				<div>
-					<select
-						id="filter-day"
-						name="filter-day"
-						aria-label="Select by Day"
-						value={filterDay}
-						onChange={e => setFilterDay(e.target.value)}
-					>
-						<option value="">Select by Day</option>
-						<option value="mon" data-filter="mon">
-							Monday
-						</option>
-						<option value="tue" data-filter="tue">
-							Tuesday
-						</option>
-						<option value="wed" data-filter="wed">
-							Wednesday
-						</option>
-						<option value="thu" data-filter="thu">
-							Thursday
-						</option>
-					</select>
-				</div>
-				<div>
-					<select
-						id="filter-track"
-						name="filter-track"
-						aria-label="Select by Track"
-						value={filterTrack}
-						onChange={e => setFilterTrack(e.target.value)}
-					>
-						<option value="">Select by Track</option>
-						<option value="training" data-filter="training">
-							Training Day
-						</option>
-						<option value="general" data-filter="general">
-							General
-						</option>
-						<option value="create" data-filter="create">
-							Create
-						</option>
-						<option value="share" data-filter="share">
-							Share
-						</option>
-						<option value="integrate" data-filter="integrate">
-							Integrate
-						</option>
-						<option value="business" data-filter="business">
-							Business
-						</option>
-						<option value="sponsor" data-filter="sponsor">
-							Sponsor
-						</option>
-						<option value="fba" data-filter="fba">
-							FBA
-						</option>
-					</select>
-				</div>
-				<div>
-					<Link
-						to={
-							this.props.location.pathname === '/schedule' ? '/schedule' : '/'
-						}
-						id="btn-expand-all"
-						onClick={e => toggleExpandAll(e.target.value)}
-					>
-						{expandAll ? 'Collapse All' : 'Expand All'}
-					</Link>
-				</div>
-			</form>
+			</>
 		);
 	}
 
